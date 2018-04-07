f041249d3e2abcd312f86641ac7247a62803e52e
muranxiao <31270586+muranxiao@users.noreply.github.com>
2018-04-07 16:43:31 -0700

    insertion and merge

--- src/sorting/SortingAlgorithms.java
+++ src/sorting/SortingAlgorithms.java
@@ -15,6 +15,24 @@ public class SortingAlgorithms  implements SortInterface {
     @Override
     public void insertionSort(Comparable[] array, int lowindex, int highindex, boolean reversed) {
         // FILL ON CODE
+        Comparable curr;
+        int j;
+        for (int i = lowindex; i < highindex; i++) {
+            curr = array[i];
+            j = i - 1;
+            if(reversed){
+                while (j >= lowindex && array[j].compareTo(curr) < 0) {
+                    array[j + 1] = array[j]; // shift elems to the right
+                    j--;
+                }
+            }else {
+                while (j >= lowindex && array[j].compareTo(curr) > 0) {
+                    array[j + 1] = array[j]; // shift elems to the right
+                    j--;
+                }
+            }
+            array[j + 1] = curr;
+        }
     }
 
     /**
@@ -25,6 +43,68 @@ public class SortingAlgorithms  implements SortInterface {
     @Override
     public void iterativeMergeSort(Comparable[] array) {
         // FILL ON CODE
+        Comparable[] temp = new Comparable[array.length];
+        mergeSort(array, temp);
+    }
+
+    /**
+     * A private mergeSort method - takes an array, a temporary array, and the
+     * indices that specify what part of the list we are working with (we need
+     * to sort the part from low to high)
+     *
+     * @param arr
+     * @param temp
+     */
+    private static void mergeSort(Comparable[] arr, Comparable[] temp) {
+        int length = 2;
+        while(length <= arr.length){
+            int times = arr.length / length;
+            for(int i = 0; i < times; i++){
+                int low = i * length;
+                int high = low + length - 1;
+                int mid = (low + high) / 2;
+                merge(arr, temp, low, mid, high);
+            }
+            length = length * 2;
+        }
+    }
+
+    /**
+     * Merge two sorted sublists together, one that goes from low to mid another
+     * goes from mid+1 to high. Uses a temporary array.
+     *
+     * @param arr
+     * @param temp
+     * @param low
+     * @param mid
+     * @param high
+     */
+    public static void merge(Comparable[] arr, Comparable[] temp, int low, int mid, int high) {
+        int k = low;
+        int i = low;
+        int j = mid + 1;
+        while (k <= high) {
+            if (i > mid) {// ran out of elements in the i sublist
+                temp[k] = arr[j];
+                k++;
+                j++;
+            } else if (j > high) {// ran out of elements in the j sublist
+                temp[k] = arr[i];
+                k++;
+                i++;
+            } else if (arr[i].compareTo(arr[j]) < 0) { // place arr[i] in temp, move i
+                temp[k] = arr[i];
+                k++;
+                i++;
+            } else {
+                temp[k] = arr[j]; // place arr[j] in temp, move j
+                k++;
+                j++;
+            }
+        }
+        // copy the result from temp back to arr
+        for (k = low; k <= high; k++)
+            arr[k] = temp[k];
     }
 
     /**
