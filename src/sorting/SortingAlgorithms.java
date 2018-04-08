4bc5acb3c622d83ef2d4ea967b4f5cf279ce8ee1
muranxiao <31270586+muranxiao@users.noreply.github.com>
2018-04-07 21:51:36 -0700

    heap sort

--- src/sorting/SortingAlgorithms.java
+++ src/sorting/SortingAlgorithms.java
@@ -118,7 +118,9 @@ public class SortingAlgorithms  implements SortInterface {
     @Override
     public void heapSort(Comparable[] array, int lowindex, int highindex) {
         // FILL ON CODE
-
+        MaxHeap mh = new MaxHeap(array, lowindex, highindex);
+        int size = highindex - lowindex;
+        for(int i = 0; i <= size - 1; i++) mh.removeMax();
     }
 
     /**
@@ -167,6 +169,130 @@ public class SortingAlgorithms  implements SortInterface {
      */
     public void externalSort(String inputFile, String outputFile, int n, int m) {
         // FILL IN CODE
+
+    }
+
+    private class MaxHeap {
+        private Comparable[] heap; // the array to store the heap
+//        private int maxsize; // the size of the array
+        private int size; // the current number of elements in the array
+        private int low;
+        private int high;
+
+        /**
+         * the constructor
+         * @param arr
+         */
+        public MaxHeap(Comparable[] arr, int low, int high) {
+//            maxsize = max;
+            heap = arr;
+            this.low = low;
+            this.high = high;
+            size = high - low;
+            buildBottomUp();
+            // Note: no actual data is stored at heap[0].
+            // Assigned MIN_VALUE so that it's easier to bubble up
+        }
+
+        private void buildBottomUp(){
+            int end = high - 1;
+            int start = parent(end);
+            while(start != end){
+                for(int i = start + 1; i <= end; i++) pushdown(i);
+                end = start;
+                start = parent(end);
+            }
+            pushdown(low);
+        }
+
+        /** Return the index of the left child of the element at index pos
+         *
+         * @param pos the index of the element in the heap array
+         * @return the index of the left child
+         */
+        private int leftChild(int pos) {
+            return 2 * (pos - low) + 1 + low;
+        }
+
+        /** Return the index of the right child
+         *
+         * @param pos the index of the element in the heap array
+         * @return the index of the right child
+         */
+        private int rightChild(int pos) {
+            return 2 * (pos - low) + 2 + low;
+        }
+
+        /** Return the index of the parent
+         *
+         * @param pos the index of the element in the heap array
+         * @return the index of the parent
+         */
+        private int parent(int pos) {
+            return (((pos - low) - 1) / 2) + low;
+        }
+
+        /** Returns true if the node in a given position is a leaf
+         *
+         * @param pos the index of the element in the heap array
+         * @return true if the node is a leaf, false otherwise
+         */
+        private boolean isLeaf(int pos) {
+            return ((pos > ((size / 2) - 1 + low)) && ((pos - low + 1) <= size));
+        }
+
+        /** Swap given elements: one at index pos1, another at index pos2
+         *
+         * @param pos1 the index of the first element in the heap
+         * @param pos2 the index of the second element in the heap
+         */
+        private void swap(int pos1, int pos2) {
+            Comparable tmp;
+            tmp = heap[pos1];
+            heap[pos1] = heap[pos2];
+            heap[pos2] = tmp;
+        }
+
+        /** Print the array that stores the heap */
+        public void print() {
+            int i;
+            for (i = 1; i <= size; i++)
+                System.out.print(heap[i] + " ");
+            System.out.println();
+        }
+
+        /** Remove minimum element (it is at the top of the minHeap)
+         *
+         * @return the smallest element in the heap
+         */
+        public void removeMax() {
+            System.out.println("low is " + low + " high is " + high + " size is " + size);
+            swap(low, size + low - 1); // swap the end of the heap into the root
+            size--;  	   // removed the end of the heap
+            // fix the heap property - push down as needed
+            if (size != 0)
+                pushdown(low);
+        }
+
+        /** Push the value down the heap if it does not satisfy the heap property
+         *
+         * @param position the index of the element in the heap
+         */
+        private void pushdown(int position) {
+            int largestChild;
+            while (!isLeaf(position)) {
+                largestChild = leftChild(position); // set the smallest child to left child
+                if ((largestChild < size + low - 1) && (heap[largestChild].compareTo(heap[largestChild + 1]) < 0))
+                    largestChild = largestChild + 1; // right child was smaller, so smallest child = right child
+
+                // the value of the smallest child is less than value of current,
+                // the heap is already valid
+                if (heap[position].compareTo(heap[largestChild]) >= 0)
+                    return;
+                swap(position, largestChild);
+                position = largestChild;
+            }
+        }
 
     }
 }
